2c6033e82a66c47aad9083c0980888d43237fa7b
Sugaroverflow <sugaroverflow@gmail.com>
2018-10-17 15:43:08 -0400

    WIP

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ Drones Team
 """
 
 import os
+import json
+import sys
 from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 import pandas as pd
+import numpy as np
 is_prod = os.environ.get('IS_HEROKU', None)
 
 # Create a new instance of the Flask class.
 app = Flask(__name__, template_folder=".")
 
 # Set API key based on environment.
-if is_prod:
-  app.config['GOOGLEMAPS_KEY'] = os.environ['api_key']
-else:
-  import config
-  app.config['GOOGLEMAPS_KEY'] = config.api_key
+import config
+app.config['GOOGLEMAPS_KEY'] = config.api_key
 
 # Initialize the extension.
 GoogleMaps(app)
@@ -27,17 +27,16 @@ GoogleMaps(app)
 # map the URL / to the function mapview()
 @app.route("/")
 def mapview():
+    # initialize
+    get_data()
     # creating a map in the view
     gmap = Map(
-        identifier="theMap",
+        identifier="map",
         lat=56.1304,
         lng=-106.3468,
     )
-    # Read the data from the csv.
-    data = pd.read_csv('data/merged-data.csv')
-
     # Uses a Flask function to render the home.html template
-    return render_template('templates/home.html', theMap=gmap)
+    return render_template('templates/home.html', map=gmap)
 
 def get_data():
     '''
@@ -60,12 +59,70 @@ def get_data():
     merged_inner = pd.merge(left=ap_sub,right=rw_sub, left_on='id', right_on='airport_ref')
     # merge with frequencies
     merged_full = pd.merge(left=merged_inner, right=frq_sub, left_on='id_x', right_on='airport_ref')
-    # save to a file
-    merged_full.to_csv('data/sprint-data.csv', encoding='utf-8', index=False)
+    # subset of columns
+    cols = ['id_x', 'type_x', 'latitude_deg', 'longitude_deg', 'length_ft', 'width_ft', 'surface', 'frequency_mhz']
+    merged_sub = merged_full[cols]
+    # rename some columns
+    merged_sub = merged_sub.rename(
+      columns={
+        'id_x':'airport_id',
+        'type_x':'airport_type',
+        'latitude_deg':'airport_long',
+        'longitude_deg':'airport_lat',
+        'length_ft':'runway_len',
+        'width_ft':'runway_wdt',
+        'surface':'runway_surface',
+        'frequency_mhz':'radio_freq'
+      }
+    )
+    # save to csv
+    merged_sub.to_csv('data/sprint-data.csv', encoding='utf-8', index=False)
+
+    # save to json
+    json_result_string = merged_sub.to_json(
+        orient='records',
+        double_precision=12,
+        date_format='iso'
+    )
+    constructGeoJson(json_result_string)
+
+
+def constructGeoJson(json_result_string):
+    '''
+    for le googlemaps API
+    '''
+    json_result = json.loads(json_result_string)
+
+    geojson = {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+
+    for record in json_result:
+        geojson['features'].append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'icon': '../img/map-icon-test.jpg',
+                'coordinates': [record['airport_lat'], record['airport_long']]
+            },
+            'properties': {
+              'airport_id': record['airport_id'],
+              'airport_type': record['airport_type'],
+              'airport_lat': record['airport_lat'],
+              'airport_long': record['airport_long'],
+              'runway_len': record['runway_len'],
+              'runway_wdt': record['runway_wdt'],
+              'runway_surface': record['runway_surface'],
+              'radio_freq': record['radio_freq']
+            }
+        })
+
+    with open('static/data/dataset.json', 'w') as f:
+        f.write(json.dumps(geojson, indent=2))
 
 if __name__ == "__main__":
-    # Initialize the geocoder.
 
-    get_data()
+    # get_data()
     # We use run() to run our app on a local server.
     app.run(debug=True)
--- static/js/mapFunctions.js
+++ static/js/mapFunctions.js
@@ -1,13 +1,13 @@
 var map;
 
 function initMap() {
-  console.log("YO initialize le map!");
 
   var myLatLng = {lat: 56.1304, lng: -106.3468};
 
-  var map = new google.maps.Map(document.getElementById('theMap'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: myLatLng
+    // center: new google.maps.LatLng(-75.296943664551,45.391666412354)
   });
 
   var marker = new google.maps.Marker({
@@ -15,5 +15,7 @@ function initMap() {
     map: map,
     title: 'Hello World!'
   });
+
+  map.data.loadGeoJson('static/data/test.json');
 }
 
